447d20f54e6b1c260b9e2064ab7f2ce35c03c352
Jian Han <jianhan@mail.com>
2020-01-16 19:37:38 +1000

    Fix allowed origin error

--- services/s3/main.tf
+++ services/s3/main.tf
@@ -57,7 +57,7 @@ resource "aws_s3_bucket" "terraform_state" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["PUT", "POST", "GET"]
-    allowed_origins = [*]
+    allowed_origins = ["*"]
     expose_headers  = ["ETag"]
     max_age_seconds = 6000
   }
