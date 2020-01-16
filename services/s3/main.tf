4d5b7a5d404ff0163960c76199e8cdf21ad22a2e
Jian Han <jianhan@mail.com>
2020-01-16 19:28:38 +1000

    Added cross origin configs for s3

--- services/s3/main.tf
+++ services/s3/main.tf
@@ -54,6 +54,14 @@ resource "aws_s3_bucket" "terraform_state" {
     }
   }
 
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "GET"]
+    allowed_origins = [*]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 6000
+  }
+
   # tags
   tags = merge(
   {
