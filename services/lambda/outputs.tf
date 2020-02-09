1fbb97b1c95853f8c9b934325f2d7b2b2217ec2d
Jian Han <jianhan@mail.com>
2020-02-09 11:24:52 +1000

    Added target id to output variable

--- services/lambda/outputs.tf
+++ services/lambda/outputs.tf
@@ -7,3 +7,8 @@ output "lambda_function_name" {
   value = aws_lambda_function.lambda_function.function_name
   description = "Lambda function name"
 }
+
+output "lambda_function_target_id" {
+  value = aws_lambda_function.lambda_function.id
+  description = "Lambda function target id"
+}
